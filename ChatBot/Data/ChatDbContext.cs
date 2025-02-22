19b2987855d300dd0008a6f9731f41d47dcd5f2b
Barabasz <barbarski@interia.eu>
2025-02-22 12:08:18 +0100

    Add project files.

---
+++ ChatBot/Data/ChatDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChatBot.Data
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public bool IsUserMessage { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public int? Rating { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+    public class ChatDbContext : DbContext
+    {
+        public DbSet<ChatMessage> ChatMessages => Set<ChatMessage>();
+
+        public ChatDbContext(DbContextOptions<ChatDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ChatMessage>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+            });
+        }
+    }
+}
